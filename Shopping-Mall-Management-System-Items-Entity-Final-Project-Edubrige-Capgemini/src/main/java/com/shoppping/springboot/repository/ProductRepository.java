17f264071337c69fce5abaa4f4c0ce60afe56297
sujithsharma <36754084+sujithsharma@users.noreply.github.com>
2021-09-22 22:12:38 +0530

    Add files via upload

---
+++ Shopping-Mall-Management-System-Items-Entity-Final-Project-Edubrige-Capgemini/src/main/java/com/shoppping/springboot/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.shoppping.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shopping.springboot.model.Product;
+
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
---
+++ Shopping-Mall-Management-System-Items-Entity-Final-Project-Edubrige-Capgemini/src/main/java/com/shoppping/springboot/service/ProductService.java
@@ -0,0 +1,16 @@
+package com.shoppping.springboot.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.shopping.springboot.model.Product;
+
+
+public interface ProductService {
+	List<Product> getAllProducts();
+	void saveProduct(Product product);
+	Product getProductById(long id);
+	void deleteProductById(long id);
+	Page<Product> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
