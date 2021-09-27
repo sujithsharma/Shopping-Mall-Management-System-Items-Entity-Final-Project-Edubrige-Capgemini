2865b73d3af04e15227b29878ff606f4b6ecdb88
sujithsharma <36754084+sujithsharma@users.noreply.github.com>
2021-09-27 12:53:35 +0530

    Add files via upload

---
+++ Shopping-Mall-Management-System-Items-Entity-Final-Project-Edubrige-Capgemini/src/main/java/com/shopping/mall/ShoppingMallManagementSystemItemsEntityFinalProjectEdubrigeCapgeminiApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallManagementSystemItemsEntityFinalProjectEdubrigeCapgeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMallManagementSystemItemsEntityFinalProjectEdubrigeCapgeminiApplication.class, args);
+	}
+
+}
