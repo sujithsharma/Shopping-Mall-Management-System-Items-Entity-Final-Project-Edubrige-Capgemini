f381e74717a723d629a41b714b7b064eb84beb2b
sujithsharma <36754084+sujithsharma@users.noreply.github.com>
2021-09-27 11:31:13 +0530

    Add files via upload

---
+++ ShoppingMall- Items entity Using Springboot-Final Project-Capgemini/src/main/java/com/shopping/mall/SpringbootProductWeb1Application.java
@@ -0,0 +1,15 @@
+package com.shopping.mall;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProductWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProductWeb1Application.class, args);
+	}
+
+}
+
