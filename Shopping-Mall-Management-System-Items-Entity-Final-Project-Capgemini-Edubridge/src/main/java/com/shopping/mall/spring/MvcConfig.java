d0e4e8494d5bcc407821d4299a4eafd9ba855a88
sujithsharma <36754084+sujithsharma@users.noreply.github.com>
2021-10-22 08:24:53 +0530

    Add files via upload

---
+++ Shopping-Mall-Management-System-Items-Entity-Final-Project-Capgemini-Edubridge/src/main/java/com/shopping/mall/spring/MvcConfig.java
@@ -0,0 +1,37 @@
+package com.shopping.mall.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+	
+	public MvcConfig()
+	{
+		super();
+	}
+	@Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+		 registry.addViewController("/addProduct.html");
+		    
+    	
+	}
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry)
+	{
+		registry.addResourceHandler("/**",
+        		"/css/**",
+                "/js/**",
+                "/api/**"               
+               )
+		        .addResourceLocations(
+        		   "classpath:/static/css/",
+        		   "classpath:/static/js/",
+        		   "classpath:/static/api/"
+        		   );
+	}
+}
